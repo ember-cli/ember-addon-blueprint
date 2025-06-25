342ca9e61f4c8795e8c83b7d222ba2cf9c3098c6
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-06-25 12:04:29 -0400

    Add comment to explain strategy

--- tests/fixtures/build-mode-tests/debug-utils-test.js
+++ tests/fixtures/build-mode-tests/debug-utils-test.js
@@ -4,6 +4,8 @@ import { assert } from '@ember/debug';
 module('debug utils remain in the build', function () {
   test('debug utils work in tests', function(qAssert) {
     qAssert.throws(() => {
+      // If we get the build mode wrong, e.g.: `NODE_ENV` != 'development'
+      //   then the assert will be stripped and qAssert will not detect an error being thrown
       assert('it works', false);
     }, 'it works');
   });
