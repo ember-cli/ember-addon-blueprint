1c24206982b2ccec639fe7e8d45fcdbd7fd5364e
Alexey Kulakov <uni_que@me.com>
2025-10-05 19:33:44 -0700

    remove registry declarations

--- tests/fixtures/explicit-imports/test-app/types/registry.d.ts
+++
@@ -1,6 +0,0 @@
-import { default as MyAddonRegistry } from 'my-addon/template-registry';
-
-declare module '@glint/environment-ember-loose/registry' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-  export default interface Registry extends MyAddonRegistry {}
-}
