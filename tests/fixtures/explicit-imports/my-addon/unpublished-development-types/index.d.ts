4d2307e5b8ad6009e8f890b977bda5f3c9712524
Alexey Kulakov <uni_que@me.com>
2025-10-05 19:07:41 -0700

    chore: remove `@glint/environment-ember-*`

--- tests/fixtures/explicit-imports/my-addon/unpublished-development-types/index.d.ts
+++ tests/fixtures/explicit-imports/my-addon/unpublished-development-types/index.d.ts
@@ -1,8 +1,6 @@
 // Add any types here that you need for local development only.
 // These will *not* be published as part of your addon, so be careful that your published code does not rely on them!
 
-import '@glint/environment-ember-loose';
-
 import { default as MyRegistry } from '../src/template-registry';
 
 declare module '@glint/environment-ember-loose/registry' {
--- tests/fixtures/explicit-imports/test-app/types/registry.d.ts
+++ tests/fixtures/explicit-imports/test-app/types/registry.d.ts
@@ -1,6 +1,3 @@
-import '@glint/environment-ember-loose';
-import '@glint/environment-ember-template-imports';
-
 import { default as MyAddonRegistry } from 'my-addon/template-registry';
 
 declare module '@glint/environment-ember-loose/registry' {
